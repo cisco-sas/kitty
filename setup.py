22802e0100d3a1e3b6417be40e16380a35e223e6
Binyamin Sharet <s.binyamin@gmail.com>
2016-03-20 17:02:51 +0200

    version 0.6.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-VERSION = '0.6.4'
+VERSION = '0.6.5'
 AUTHOR = 'Cisco SAS team'
 EMAIL = 'kitty-fuzzer@googlegroups.com'
 URL = 'https://github.com/cisco-sas/kitty.git'
@@ -39,7 +39,7 @@ else:
         author_email=EMAIL,
         url=URL,
         packages=find_packages(),
-        install_requires=['docopt', 'bitstring', 'six', 'requests'],
+        install_requires=['docopt', 'bitstring!=3.1.4', 'six', 'requests'],
         keywords='fuzz framework sulley kitty',
         entry_points={
             'console_scripts': [
