d7b3e947a4f224ea526aeef9fea3a4368dc1482a
Binyamin Sharet <s.binyamin@gmail.com>
2019-02-17 10:56:09 +0200

    version 0.7.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-VERSION = '0.7.1'
+VERSION = '0.7.2'
 AUTHOR = 'Cisco SAS team'
 EMAIL = 'kitty-fuzzer@googlegroups.com'
 URL = 'https://github.com/cisco-sas/kitty.git'
